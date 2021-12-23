ff6ae4cf877c2c94d29712859bd8902ce19efe30
atreyakala <atreya_kala@yahoo.co.in>
2021-12-24 02:45:54 +0530

    feat(user security): add SecurityConfiguration and seed users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user (id, user_name, password, active, roles)
+values (1, 'john', 'john', true, 'USER'),
+       (2, 'alan', 'alan', true, 'USER'),
+       (3, 'lucy', 'lucy', true, 'USER');
+
+
