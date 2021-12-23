71528a9bef750abb8fe7766ee9a78fac42d37048
atreyakala <atreya_kala@yahoo.co.in>
2021-12-24 00:59:46 +0530

    initial commit

---
+++ src/main/java/com/atreya/resumeapp/ResumeAppApplication.java
@@ -0,0 +1,13 @@
+package com.atreya.resumeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResumeAppApplication.class, args);
+  }
+
+}
